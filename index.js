98aaf34c4a21e5cad7695b98db3d68463b8f6d46
Luis Enrique Guerrero <luisenguerrero@yahoo.com>
2021-12-06 22:33:20 -0500

    Update Backend for deploy

--- index.js
+++ index.js
@@ -4,19 +4,25 @@ var mongoose = require('mongoose');
 var app = require('./App');
 var port = 3900;
 
+const database = "Colosus_db";
+const password = "666JyzE3nEK.UDx";
+const URI = 'mongodb+srv://StarFiveTeam:'+password+'@akasha-db.cpnnt.mongodb.net/'+database+'?retryWrites=true&w=majority';
+
+
 mongoose.set('useFindAndModify', false); //DESACTIVA LOS METODOS ANTIGUOS DE CONEXIÓN
 mongoose.Promise = global.Promise; //ESTABLECE LA PROMESA GLOBAL
 
 //REALIZA LA CONEXION DE LA BD CON NODE.JS
 // mongoose.connect('mongodb://localhost:27017/api_rest_blog',{useUnifiedTopology: true, useNewUrlParser: true}) // SERVICIO LOCAL
-mongoose.connect('mongodb://technology-blog.netlify.app:27017/api_rest_blog',{useUnifiedTopology: true, useNewUrlParser: true})  // SERVICIO ON-LINE
+mongoose.connect(URI)  // SERVICIO ATHLAS
         .then(() => {
             console.log('La conexión con MongooDB se establecio en forma Exitosa!!!');
 
             // CREAR EL SERVIDOR Y ESCUCHAR PETICIONES:
             app.listen(port, () =>{
-                console.log('Servidor corriendo en:https://technology-blog.netlify.app:'+port); // SERVICIO ON-LINE
-                //console.log('Servidor corriendo en: Http://localhost:'+port); // SERVICIO LOCAL
+                //console.log('Servidor corriendo en:https://technology-blog.netlify.app:'+port); // SERVICIO ON-LINE
+                console.log('Servidor corriendo en: Http://localhost:'+port); // SERVICIO LOCAL
             });
 
-        });
+        })
+        .catch(err => console.err(err));
